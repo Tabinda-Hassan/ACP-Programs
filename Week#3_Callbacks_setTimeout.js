2f69942189dc6e4fbe7a3373a5e131d630a36615
Tabinda-Hassan <tabindahassan279@gmail.com>
2025-02-24 20:25:05 +0500

    JavaScript Code

--- Week#3_Callbacks_setTimeout.js
+++
@@ -1,8 +0,0 @@
-function fetchData(callback)
-{
-    setTimeout(() => {
-        console.log("Data received");
-        callback();
-    }, 2000);
-}
-fetchData(() => console.log("Processing data"));
